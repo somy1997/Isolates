b11fe4b73011aab0e7143dcc7954f8e3e63d7ce5
Nishant Baranwal Somy <somy1997@gmail.com>
2020-01-26 18:55:02 +0530

    SOF method

--- System call intercept via LKM/intercept_unlink/intercept_unlink.c
+++ System call intercept via LKM/intercept_unlink/intercept_unlink.c
@@ -19,23 +19,27 @@ asmlinkage long hacked_sys_unlink(const char *pathname)
         
 static int __init my_init (void)
 {
+	printk(KERN_ALERT "Entered my_init\n");
         /*obtain sys_call_table from hardcoded value
         we found in System.map*/
-        *(long *)&sys_call_table=0xffffffff81e001e0;
+        sys_call_table=0xffffffff81e001e0;
+        sys_call_table=0x81e001e0;
         
+	printk(KERN_ALERT "Assigned address to sys_call_table, calling xchg now\n");
         /*store original location of sys_unlink. Alter sys_call_table
         to point _ _NR_unlink to our hacked_sys_unlink*/
         original_sys_unlink =(void * )xchg(&sys_call_table[__NR_unlink],
 hacked_sys_unlink);
-        
+        printk(KERN_ALERT "xchg called successfully, original sys unlink swapped with hacked sys unlink\n");
         return 0;
 }
 
 static void my_exit (void)
 {
 /*restore original sys_unlink in sys_call_table*/
+	printk(KERN_ALERT "Entered my_exit\n");
         xchg(&sys_call_table[__NR_unlink], original_sys_unlink);
-
+	printk(KERN_ALERT "xchg called successfully, original sys unlink restored\n");
 }       
 
 module_init(my_init);
